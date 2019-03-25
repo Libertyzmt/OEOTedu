a54b18087eedab85dfd0b7121c3666fb49c71f83
赵猛彤 <1183525873@qq.com>
2019-03-25 11:33:16 +0800

    删除部分无用注释

--- training/views.py
+++ training/views.py
@@ -17,12 +17,6 @@ def index(request):
     return render(request, 'training/index.html', {'posts': posts, 'depas': depas})
 
 
-# # 课程列表
-# def courses_list(request):
-#     courses = Course.objects.all()
-#     return render(request, 'training/courses_list.html', {'courses': courses})
-
-
 def course_failed(request):
     return render(request, 'training/course_failed.html')
 
@@ -154,7 +148,6 @@ def post_delete(request, id):
         return HttpResponse('当前登录用户没有权限，请切换用户或者联系管理员.')
 
 
-
 # 班级人员列表——艾鹏
 def profile_list(request, id):
     department = Department.objects.get(id=id)
@@ -167,9 +160,6 @@ def profile_list(request, id):
 def section_list(request):
     look = Department.objects.filter(name__contains="部")
     return render(request, 'training/department/look_section.html', {'look': look})
-
-
-
 
 
 # 部门下的人员__斌
@@ -282,13 +272,15 @@ def user_like(request):
         except:
             pass
     return JsonResponse({'status': 'ko'})
-#最新课程（可报名）--王凯杰
+
+
+# 最新课程（可报名）--王凯杰
 def new_course(request):
-    new_courses=Course.objects.all().order_by('starttime')
-    course_list=[]
+    new_courses = Course.objects.all().order_by('starttime')
+    course_list = []
     for course in new_courses:
-        c=course.students.count()
-        if c<course.most:
+        c = course.students.count()
+        if c < course.most:
             course_list.append(course)
 
-    return render(request,'training/new_courses.html',{'course_list':course_list})
+    return render(request, 'training/new_courses.html', {'course_list': course_list})
